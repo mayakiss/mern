53711ede8271783a4d10527227c9111acc0256fd
issa <iss.tankary@gmail.com>
2021-02-24 17:06:42 +0100

    Projets Javascprit

--- ex7.js
+++ ex7.js
@@ -1,1 +1,17 @@
 import { SCRIPTS } from "./script.js";
+
+function dominantDirection(string){
+    for(string of letter){
+        
+    }
+}
+function(SCRIPTS){
+    SCRIPTS.ranges[]
+    for(script of SCRIPTS){
+
+    }
+}
+function(){
+
+}
+console.log(dominantDirection("Issa"))
